063042dfb0a1c0100ad271b7edc1438634d5fde3
Janne Mäkinen <bacrguy@gmail.com>
2015-10-18 20:07:50 +0200

    Monsters addedgit status At least the class...

--- include/MonsterCard.h
+++ include/MonsterCard.h
@@ -1,13 +1,23 @@
 #ifndef MONSTERCARD_H
 #define MONSTERCARD_H
 #include "Card.h"
+#include <string>
 
 class MonsterCard:Card
 {
     public:
+        void AddAttack(int attack);
+        void ReduceDefence(int defence);
+        void SetCardAttack(int setattack);
+        void SetCardDefence(int setdefence);
+        int GetCardAttack();
+        int GetCardDefence();
         MonsterCard();
         virtual ~MonsterCard();
     protected:
+        int card_attack;
+        int card_defence;
+        std::string card_ability;
     private:
 };
 
--- src/MonsterCard.cpp
+++ src/MonsterCard.cpp
@@ -2,10 +2,65 @@
 
 MonsterCard::MonsterCard()
 {
-    //ctor
+    card_ability = "no text"; //ctor
 }
 
 MonsterCard::~MonsterCard()
 {
     //dtor
 }
+
+//Get and Set functions
+void MonsterCard::SetCardAttack(int setattack)
+{
+  card_attack = setattack;
+}
+
+int MonsterCard::GetCardAttack()
+{
+  return card_attack;
+}
+
+void MonsterCard::SetCardDefence(int setdefence)
+{
+  card_defence = setdefence;
+}
+
+int MonsterCard::GetCardDefence()
+{
+  return card_defence;
+}
+
+//Attack and Defence additions and reductions
+void MonsterCard::AddAttack(int attack);
+{
+  card_attack = card_attack + attack;
+}
+
+void MonsterCard::ReduceAttack(int attack);
+{
+  card_attack = card_attack - attack;
+
+void MonsterCard::AddDefence(int defence);
+{
+  card_defence = card_defence + defence;
+}
+
+void MonsterCard::ReduceDefence(int defence);
+{
+  card_defence = card_defence - defence;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
