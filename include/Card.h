f60e2023aec360768bdac9b803bcccc14ff8da63
Janne Mäkinen <bacrguy@gmail.com>
2015-10-18 17:41:02 +0200

    Card Class added

--- include/Card.h
+++ include/Card.h
@@ -1,14 +1,26 @@
 #ifndef CARD_H
 #define CARD_H
-
+#include <string>
 
 class Card
 {
     public:
         Card();
+        std::string GetCardName();
+        void SetCardName(std::string type1);
+        std::string GetCardType();
+        void SetCardType(std::string type1);
         virtual ~Card();
+        std::string card_class;
+        void SetCastingCost(int cost);
+        int GetCastingCost();
     protected:
+        int casting_cost;
+        std::string card_text;
+        std::string card_name;
+        std::string card_type;
     private:
+
 };
 
 #endif // CARD_H
--- src/Card.cpp
+++ src/Card.cpp
@@ -2,10 +2,63 @@
 
 Card::Card()
 {
-    //ctor
+ card_class = "basic";
+ card_text = "default text";//ctor
+
+
+
 }
 
 Card::~Card()
 {
     //dtor
 }
+
+std::string Card::GetCardName()
+{
+  return card_name;
+}
+
+void Card::SetCardName(std::string name1)
+{
+  card_name = name1;
+}
+
+std::string Card::GetCardType()
+{
+  return card_type;
+}
+
+void Card::SetCardType(std::string type1)
+{
+  card_type = type1;
+}
+
+void Card::SetCastingCost(int cost)
+{
+  casting_cost = cost;
+}
+
+int Card::GetCastingCost()
+{
+  return casting_cost;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
