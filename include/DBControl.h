6bf795006a4a9caf80453033f202ddf496525918
jrutanen <jani.m.rutanen@gmail.com>
2015-12-05 20:20:47 +0100

    adding sqlite support

---
+++ include/DBControl.h
@@ -0,0 +1,14 @@
+#ifndef DBCONTROL_H
+#define DBCONTROL_H
+
+
+class DBControl
+{
+  public:
+    DBControl();
+    virtual ~DBControl();
+  protected:
+  private:
+};
+
+#endif // DBCONTROL_H
---
+++ src/DBControl.cpp
@@ -0,0 +1,12 @@
+#include "DBControl.h"
+#include "sqlite3.h"
+
+DBControl::DBControl()
+{
+  //ctor
+}
+
+DBControl::~DBControl()
+{
+  //dtor
+}
